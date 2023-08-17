142e43dac5d8bbbe06a5e18f0ab5cfefc77aa950
Lime-sisi <chloel97@protonmail.com>
2023-08-17 03:02:17 -0400

    changed test_views

--- Littlelemon/tests/test_views.py
+++ Littlelemon/tests/test_views.py
@@ -1,11 +1,11 @@
 from django.test import RequestFactory, TestCase
 from restaurant.models import menu
-from rest_framework import status
 from restaurant.views import MenuItemView
 
 
 class MenuViewTest(TestCase):
     def setUp(self):
+        #access to request factory
         self.factory = RequestFactory()
         menu.objects.create(title='sugar cloud',price = 4.2,inventory=14)
         menu.objects.create(title='strawberry hot chocolate',price = 9.2,inventory=30)
@@ -13,6 +13,7 @@ class MenuViewTest(TestCase):
     def test_getall(self):
         request = self.factory.get("/restaurant/menu")
         response = MenuItemView.as_view()(request)
+    
         self.assertEqual(response.status_code, 200)
 
         
