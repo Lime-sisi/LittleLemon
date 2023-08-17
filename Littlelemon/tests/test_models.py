84104eaec4a13884ab3b493c20f30703b05dd2bc
Lime-sisi <chloel97@protonmail.com>
2023-08-17 02:56:25 -0400

    added test_views

--- Littlelemon/tests/test_models.py
+++ Littlelemon/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from restaurant.models import menu
 
 
+
 class MenuTest(TestCase):
     def test_addnew_item(self):
         item = menu.objects.create(title = 'icy lime coffee', price = 20,inventory=40)
---
+++ Littlelemon/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import RequestFactory, TestCase
+from restaurant.models import menu
+from rest_framework import status
+from restaurant.views import MenuItemView
+
+
+class MenuViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        menu.objects.create(title='sugar cloud',price = 4.2,inventory=14)
+        menu.objects.create(title='strawberry hot chocolate',price = 9.2,inventory=30)
+
+    def test_getall(self):
+        request = self.factory.get("/restaurant/menu")
+        response = MenuItemView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+        
